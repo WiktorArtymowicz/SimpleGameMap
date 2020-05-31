6ffb5c56b49684582a8634b3485f5fa3c9c22917
Wiktor Artymowicz <kurt44pl@onet.eu>
2020-05-31 19:44:28 +0200

    Refactoring map create

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace SimpleGameMap
         static void Main(string[] args)
         {
             Random random = new Random();
-            int mapLenght = 50;
-            int mapHeight = 50;
+            int mapLenght = 10;
+            int mapHeight = 10;
             TerrainEnum[,] map = new TerrainEnum[mapLenght, mapHeight];
 
             Console.OutputEncoding = UTF8Encoding.UTF8;
@@ -21,20 +21,15 @@ namespace SimpleGameMap
                     if(i == 0 || j == 0 || i == mapLenght - 1 || j == mapHeight - 1)
                     {
                         map[i, j] = TerrainEnum.wall;
+                        Console.ForegroundColor = map[i, j].GetColor();
+                        Console.Write($"{map[i, j].GetChar()}");
                     }
                     else
                     {
                         map[i, j] = (TerrainEnum)random.Next(1, 4);
+                        Console.ForegroundColor = map[i, j].GetColor();
+                        Console.Write($"{map[i, j].GetChar()}");
                     }
-                }
-            }
-
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.ForegroundColor = map[i, j].GetColor();
-                    Console.Write($"{map[i, j].GetChar()} ");
                 }
                 Console.WriteLine();
             }
